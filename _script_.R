74a4405685a61c5656faee74c329846429499eec
Pedro J. Perez <pedro.j.perez@uv.es>
2017-12-27 11:49:42 +0100

    meto el fichero _script_.R

---
+++ _script_.R
@@ -0,0 +1,34 @@
+library(readxl)
+#dir.create("./data")
+cod_provincias <- read_excel("./data-raw/cod_provincias.xlsx")
+#save(cod_provincias, file="./data/cod_provincias.RData") #- guardamos my_data en formato .RData o .rda
+devtools::use_data(cod_provincias)
+
+#- acuerdate de documentar cada nuevo fichero de datos q añadas en el fichero:
+#- ./R./data.R
+
+devtools::document()
+#devtools::use_vignette("my_vignette")
+
+
+#----- Instalarlo dsde Local
+install.packages("C:/Users/perezp/Desktop/a_GIT_2016a/mypkgfordata", repos = NULL, type="source")
+library(mypkgfordata)
+aa <- data("cod_provincias")
+#--- instalarlo desde Github
+devtools::install_github("perezp44/mypkgfordata")
+
+
+
+#- Github
+
+cd c:/Users/perezp/Desktop/a_GIT_2016a/mypkgfordata
+git remote add origin https://github.com/perezp44/mypkgfordata.git
+
+
+git add .
+git commit --all --message "Creando el REPO"
+
+
+
+aa <- data("cod_provincias")
