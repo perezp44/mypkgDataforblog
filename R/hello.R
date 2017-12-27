b4bfd1d02cb8e6c2858c2f1644012d434fadb834
Pedro J. Perez <pedro.j.perez@uv.es>
2017-12-27 12:05:46 +0100

    la funcion hello()

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,10 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+#' Esta función solo saluda a mi blog
+#' Es q quería q se modificase el fichero NAMESPACE
+#' @export
+
 hello <- function() {
-  print("Hello, world!")
+  print("Hello, R & Flowers II!")
 }
